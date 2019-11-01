f70ba43a767e2bbaa940d200dd9db44a95a433ad
Connor <cweisenb@trinity.edu>
2019-11-01 10:41:25 -0500

    added type signatures:

--- oHello.hs
+++ oHello.hs
@@ -2,3 +2,20 @@ data Player = Black | White
 data Status = Player | Empty
 type Cell = (Int, Status) --possible wrong syntax
 type Board = ([[Cell]], Player)
+
+
+checkValid :: Board -> Cell -> Bool
+
+findCell :: Board -> Int -> Int -> Cell
+
+checkWinner :: Board -> Bool
+
+updateBoard :: Cell -> Board -> Board
+
+fancyShow :: Board -> String
+
+validMoves :: Board -> [Cell]
+
+parseString :: String -> Maybe Cell
+
+countPieces :: Player -> Board -> Int
