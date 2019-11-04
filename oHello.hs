c04513b9e9793f87d2d5182a5970414ed216594a
2018-08-22 <sopon@dias02.cs.trinity.edu>
2019-11-03 23:26:11 -0600

    updated types

--- oHello.hs
+++ oHello.hs
@@ -1,21 +1,34 @@
+
+
 data Player = Black | White
-data Status = Player | Empty
-type Cell = (Int, Status) --possible wrong syntax
-type Board = ([[Cell]], Player)
+data Status = full Player | Empty
+type Location = (Int, Int)
+type Cell = (Location, Status) --possible wrong syntax
+type Board = ([Cell], Player)
+type Game = (Board, Player)
+--makeBoard :: Player -> Board
 
 
 checkValid :: Board -> Cell -> Bool
+checkValid = undefined
 
 findCell :: Board -> Int -> Int -> Cell
+findCell = undefined
 
 checkWinner :: Board -> Bool
+checkWinner = undefined
 
 updateBoard :: Cell -> Board -> Board
+updateBoard = undefined
 
 fancyShow :: Board -> String
+fancyShow = undefined
 
 validMoves :: Board -> [Cell]
+validMoves = undefined
 
 parseString :: String -> Maybe Cell
+parseString = undefined
 
 countPieces :: Player -> Board -> Int
+countPieces = undefined
