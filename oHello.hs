8b690049d6afc6c8cd8c9aba2db36c78c515866a
2018-08-22 <rla@dias01.cs.trinity.edu>
2019-11-04 21:37:23 -0600

    checkWinner calculations good, need to calculate when game is over

--- oHello.hs
+++ oHello.hs
@@ -1,8 +1,6 @@
-
-
-data Player = Black | White
-data Status = Player | Empty
-type Location = (Int, Int)
+data Player = Black | White deriving (Show, Eq) 
+data Status = Full Player | Empty deriving (Show, Eq)
+type Location = (Integer, Integer)
 type Cell = (Location, Status) --possible wrong syntax
 type Board = ([Cell], Player)
 type Game = (Board, Player)
@@ -15,8 +13,45 @@ checkValid = undefined
 findCell :: Board -> Int -> Int -> Cell
 findCell = undefined
 
-checkWinner :: Board -> Bool
-checkWinner = undefined
+
+--checks if game is over
+--game is over when both players cannot make a move, or board is full
+--true means game is over
+gameOver :: Board -> Bool
+gameOver board = 
+    --undefined
+    if length (fst board) == 64 then True else False
+
+
+--if game is over, returns a winner
+--else return nothing
+checkWinner :: Board -> Maybe Player
+checkWinner board = 
+    let gameStatus = gameOver board
+    in if gameStatus == True then winnerIs board else Nothing
+
+--helper function that calculates winner
+--winner is player with most pieces on board
+winnerIs :: Board -> Maybe Player
+winnerIs board = 
+    let 
+        lstOfCells = fst board
+        lstOfColors = [colors | (loc, colors) <- lstOfCells]
+        count = winnerIsAux lstOfColors
+        
+    in if count == 0 then Nothing else blackOrWhite count
+    
+    where
+        blackOrWhite count = 
+            if count > 0 then Just Black
+            else Just White
+
+        --black += 1, white -= 1
+        winnerIsAux [] = 0
+        winnerIsAux (x:xs) = 
+            let recur = winnerIsAux xs
+            in if x == Full Black then 1 + recur else (-1) + recur
+
 
 updateBoard :: Cell -> Board -> Board
 updateBoard = undefined
@@ -32,3 +67,8 @@ parseString = undefined
 
 countPieces :: Player -> Board -> Int
 countPieces = undefined
+
+
+testBoard = ([((2,2), Full Black), ((1,2), Full Black)], White)
+
+fullBoard = ([((2,2), Full Black), ((1,2), Full Black), ((2,2), Full Black), ((1,2), Full Black),((2,2), Full Black), ((1,2), Full Black),((2,2), Full Black), ((1,2), Full Black),((2,2), Full Black), ((1,2), Full Black),((2,2), Full Black), ((1,2), Full Black),((2,2), Full Black), ((1,2), Full Black),((2,2), Full Black), ((1,2), Full Black),((2,2), Full Black), ((1,2), Full Black),((2,2), Full Black), ((1,2), Full Black),((2,2), Full Black), ((1,2), Full Black),((2,2), Full Black), ((1,2), Full Black),((2,2), Full Black), ((1,2), Full Black),((2,2), Full Black), ((1,2), Full Black),((2,2), Full Black), ((1,2), Full Black),((2,2), Full Black), ((1,2), Full Black),((2,2), Full Black), ((1,2), Full Black),((2,2), Full Black), ((1,2), Full Black),((2,2), Full Black), ((1,2), Full Black),((2,2), Full Black), ((1,2), Full Black),((2,2), Full Black), ((1,2), Full Black),((2,2), Full Black), ((1,2), Full Black),((2,2), Full Black), ((1,2), Full Black),((2,2), Full Black), ((1,2), Full Black),((2,2), Full Black), ((1,2), Full Black),((2,2), Full Black), ((1,2), Full Black),((2,2), Full Black), ((1,2), Full Black),((2,2), Full Black), ((1,2), Full Black),((2,2), Full Black), ((1,2), Full Black),((2,2), Full Black), ((1,2), Full Black),((2,2), Full Black), ((1,2), Full Black),((2,2), Full Black), ((1,2), Full Black)], White)
