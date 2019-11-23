5f248b909f855a677b488a76f5abe142d25a2a83
Izzy Thompson <ithompso@trinity.edu>
2019-11-23 17:41:58 -0600

    main finished

--- Main.hs
+++ Main.hs
@@ -13,18 +13,18 @@ import Text.Read
 
 
 
--- /$$$$$$$$ /$$   /$$ /$$$$$$  /$$$$$$        /$$$$$$  /$$$$$$        /$$      /$$  /$$$$$$  /$$$$$$ /$$   /$$      
--- |__  $$__/| $$  | $$|_  $$_/ /$$__  $$      |_  $$_/ /$$__  $$      | $$$    /$$$ /$$__  $$|_  $$_/| $$$ | $$      
---    | $$   | $$  | $$  | $$  | $$  \__/        | $$  | $$  \__/      | $$$$  /$$$$| $$  \ $$  | $$  | $$$$| $$      
---    | $$   | $$$$$$$$  | $$  |  $$$$$$         | $$  |  $$$$$$       | $$ $$/$$ $$| $$$$$$$$  | $$  | $$ $$ $$    
---    | $$   | $$__  $$  | $$   \____  $$        | $$   \____  $$      | $$  $$$| $$| $$__  $$  | $$  | $$  $$$$      
---    | $$   | $$  | $$  | $$   /$$  \ $$        | $$   /$$  \ $$      | $$\  $ | $$| $$  | $$  | $$  | $$\  $$$      
---    | $$   | $$  | $$ /$$$$$$|  $$$$$$/       /$$$$$$|  $$$$$$/      | $$ \/  | $$| $$  | $$ /$$$$$$| $$ \  $$      
---    |__/   |__/  |__/|______/ \______/       |______/ \______/       |__/     |__/|__/  |__/|______/|__/  \__/      
-                                                                                                                   
+-- /$$$$$$$$ /$$   /$$ /$$$$$$  /$$$$$$        /$$$$$$  /$$$$$$        /$$      /$$  /$$$$$$  /$$$$$$ /$$   /$$
+-- |__  $$__/| $$  | $$|_  $$_/ /$$__  $$      |_  $$_/ /$$__  $$      | $$$    /$$$ /$$__  $$|_  $$_/| $$$ | $$
+--    | $$   | $$  | $$  | $$  | $$  \__/        | $$  | $$  \__/      | $$$$  /$$$$| $$  \ $$  | $$  | $$$$| $$
+--    | $$   | $$$$$$$$  | $$  |  $$$$$$         | $$  |  $$$$$$       | $$ $$/$$ $$| $$$$$$$$  | $$  | $$ $$ $$
+--    | $$   | $$__  $$  | $$   \____  $$        | $$   \____  $$      | $$  $$$| $$| $$__  $$  | $$  | $$  $$$$
+--    | $$   | $$  | $$  | $$   /$$  \ $$        | $$   /$$  \ $$      | $$\  $ | $$| $$  | $$  | $$  | $$\  $$$
+--    | $$   | $$  | $$ /$$$$$$|  $$$$$$/       /$$$$$$|  $$$$$$/      | $$ \/  | $$| $$  | $$ /$$$$$$| $$ \  $$
+--    |__/   |__/  |__/|______/ \______/       |______/ \______/       |__/     |__/|__/  |__/|______/|__/  \__/
+
 --styleguide for main
- -- big money ne     
- 
+ -- big money ne
+
 main :: IO ()
 main = do
     args <- getArgs
@@ -36,17 +36,17 @@ main = do
         then putStrLn $ usageInfo "Usage: fortunes [options] [file]" options
     else do
         let fileName = if null inputs && (Interactive `notElem` flags) then "initialBoard.txt" else head inputs
-        game <- readGame fileName
-        if (Interactive `elem` flags) then undefined--interactive stuff
+        game@(board, turn) <- readGame fileName
+        if (Interactive `elem` flags) then playComputer game depth
         else if (Winner `elem` flags)
             then if verbose then verbosePrint $ bestestMove game
-                 else printMove $ bestestMove game 
-        else if (move /= Nothing) then undefined --make the move
+                 else printMove $ bestestMove game
+        else if (move /= Nothing) then updateBoard (fromJust move, turn) game
         else if verbose then verbosePrint $ bestMove game depth else printMove $ bestMove game depth
  -- main = do
  --     playGame $ initialGame
  --     putStrLn "Game Over"
-  
+
 
 
 data Flag = Help | Winner | Verbose | Interactive | Depth String | Move String deriving (Eq, Show)
@@ -59,8 +59,8 @@ options = [ Option ['h'] ["help"]   (NoArg Help)   "Print usage information and 
           , Option ['v'] ["verbose"]   (NoArg Verbose) "Outputs both the best move and how good it is."
           , Option ['i'] ["interactive"]   (NoArg Interactive) "Runs program in interactive mode."
           ]
-   
-          
+
+
 getDepth :: [Flag] -> Int
 getDepth ((Depth s):_) = read s
 getDepth (_:flags) = getDepth flags
@@ -70,6 +70,7 @@ getMove :: [Flag] -> Maybe Location
 getMove ((Move s):_) = parseStringNoIO s
 getMove (_:flags) = getMove flags
 getMove [] = Nothing
+
 
 parseStringNoIO :: String -> Maybe Location
 parseStringNoIO input = if check then (Just loc) else Nothing
@@ -82,27 +83,27 @@ parseStringNoIO input = if check then (Just loc) else Nothing
 
 
 makeMove :: Game -> IO (Maybe Game)
-makeMove game@(board, turn) = 
-    do 
+makeMove game@(board, turn) =
+    do
         str <- prompt $ (playerToString turn) ++ " enter a valid move"
         loc <- parseString str
         let move = (loc, turn)
         let newGame = updateBoard move game
-        if newGame == Nothing then makeMove game 
-        else return newGame    
+        if newGame == Nothing then makeMove game
+        else return newGame
 
 playGame :: Game -> IO String
-playGame game@(board, turn)= 
+playGame game@(board, turn)=
     if not(null $ validMoves turn game)
     then do putBoard game
             a <- makeMove game
-            playGame $ fromJust a 
-    else if not(null $ validMoves (changePlayer turn) game) 
+            playGame $ fromJust a
+    else if not(null $ validMoves (changePlayer turn) game)
          then do playGame (board, changePlayer turn)
          else return $ yayWinner game
 
 yayWinner :: Game -> String
-yayWinner game = 
+yayWinner game =
     let winner = fromJust $ winnerIs game
     in if winner == Win Black then "Player Black Wins!"
     else                        "Player White Wins!"
@@ -117,24 +118,23 @@ prompt message = do putStrLn message
 --ex input "2,4"
 --from row:0-7, column:0-7
 parseString :: String -> IO Location
-parseString str = 
+parseString str =
     do
         let splitted = splitOn "," str
         let column = stringToInt $ head splitted
         let row = stringToInt $ head $ tail splitted
         let loc = (row , column)
         return loc
-        
+
 validStrs = ["0","1","2","3","4","5","6","7"]
 
 validInput :: String -> Bool
 validInput c = c `elem` validStrs
 
 playerToString :: Player -> String
-playerToString White = "Player White" 
+playerToString White = "Player White"
 playerToString Black = "Player Black"
 
 stringToInt :: String -> Int
-stringToInt c = 
+stringToInt c =
     read c :: Int
-
